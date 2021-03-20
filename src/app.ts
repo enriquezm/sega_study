8904eb4a4cd48a47289a5c24b678bc4cd831ae98
Myles Enriquez <enri.myles@gmail.com>
2021-03-20 15:44:41 -0700

    Add tests and append to dom

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,23 @@
 import Card from "./card";
 import TestEngine from "./testEngine";
+import { createElement } from './util/dom';
 
+// create new test instance with category of 'JavaScript'
 const test1 = new TestEngine('JavaScript');
 test1.getStackCategory();
 
-// create card and add it to test's card stack
-test1.addCardToStack(new Card('Test question', 'Test answer', 'Test category'));
+// create cards and add them to test's card stack
+test1.addCardToStack(new Card('Test question 1', 'Test answer for 1', 'Category 1'));
+test1.addCardToStack(new Card('Test question 2', 'Test answer for 2', 'Category 2'));
+test1.addCardToStack(new Card('Test question 3', 'Test answer for 3', 'Category 3'));
 
-console.log('hello');
+// print out stack for testing
+test1.printStack();
+
+const testList = document.getElementById('test-list');
+
+for (let card of test1.getStack()) {
+    const listElement = createElement('li', card.getQuestion());
+    testList?.appendChild(listElement);
+}
+
